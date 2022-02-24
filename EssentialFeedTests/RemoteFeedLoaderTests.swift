008474df28f6be44bf8ca744094b62c245c85f49
Claudia Maciel <clc_80@yahoo.com>
2022-02-24 10:42:36 -0800

    Tests how many times the load function is called.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -16,13 +16,24 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
 
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLCallCount, 2)
     }
     
     // MARK: - Helpers
@@ -35,9 +46,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLCallCount = 0
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLCallCount += 1
         }
     }
 }
