25b4314162722425df7ba887f12c102bd2ac65ed
Claudia <clc_80@yahoo.com>
2022-06-20 19:33:43 -0700

    end--no-edit

--- EssentialFeedTests/Feed Caches/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Caches/CodableFeedStoreTests.swift
@@ -66,14 +66,14 @@ class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        let storeURL = storeURL()
+        let storeURL = testsSpecificStoreURL()
         try? FileManager.default.removeItem(at: storeURL)
     }
     
     override func tearDown() {
         super.tearDown()
         
-        let storeURL = storeURL()
+        let storeURL = testsSpecificStoreURL()
         try? FileManager.default.removeItem(at: storeURL)
     }
     
@@ -143,13 +143,13 @@ class CodableFeedStoreTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
-        let storeURL = storeURL()
+        let storeURL = testsSpecificStoreURL()
         let sut = CodableFeedStore(storeURL: storeURL)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
     
-    private func storeURL() -> URL {
-        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+    private func testsSpecificStoreURL() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
     }
 }
