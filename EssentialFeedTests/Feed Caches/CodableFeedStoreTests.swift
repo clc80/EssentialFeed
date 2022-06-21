663759ff31ad6b1eeefbc8c4a10cdeead10abba4
Claudia <clc_80@yahoo.com>
2022-06-20 19:32:29 -0700

    Replace production store URL with a test-specific store URL to avoid sharing state/artifacts with other parts of the system (including other tests!)

--- EssentialFeedTests/Feed Caches/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Caches/CodableFeedStoreTests.swift
@@ -150,6 +150,6 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func storeURL() -> URL {
-        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
     }
 }
